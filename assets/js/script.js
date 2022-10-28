6e47e802fa82efa949814ae3ec337de6a5339dba
NatThomson <natthomsondev@gmail.com>
2022-10-28 11:21:03 +0000

    add function structure to js

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAsnwer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubctractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivideQuestion() {
+    
+}
